86ac0704b699d2f6ed3f1008f55ec0e81aa6e1a2
Tenemo <piotr@piech.dev>
2025-11-24 21:00:00 +0100

    LED strip dimming

--- include/config.h
+++ include/config.h
@@ -1,10 +1,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define TFT_DC 3
-#define TFT_CS 18
-#define TFT_RST 38
-#define LCD_BL 21
-#define LED_PIN 8
+#define TFT_DC 3         // Display DC pin
+#define TFT_CS 18        // Display CS pin
+#define TFT_RST 38       // Display RST pin
+#define LCD_BL 21        // Display backlight pin
+#define STATUS_LED_PIN 8 // Status LED pin
 
 #endif // CONFIG_H
