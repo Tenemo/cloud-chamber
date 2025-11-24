66aaf9cabd2cf3ae269c35b09ff26fe4fbb9cecf
Tenemo <piotr@piech.dev>
2025-11-24 21:00:00 +0100

    setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
+#include "esp_log.h"
+
 #include "Display.h"
 #include "config.h"
 #include <Arduino.h>
@@ -5,6 +8,7 @@
 Display display(TFT_DC, TFT_CS, TFT_RST, LCD_BL);
 
 void setup() {
+    esp_log_level_set("*", ESP_LOG_ERROR);
     Serial.begin(115200);
     Serial.println("Initializing...");
 
@@ -13,19 +17,14 @@ void setup() {
     display.begin();
 
     display.printLine("Cloud Chamber", 0, 0);
-    display.printLine("LED Blink Demo", 0, 10);
 
     Serial.println("Initialized.");
 }
 
 void loop() {
     digitalWrite(LED_PIN, HIGH);
-    display.printLine("LED ON", 0, 30, COLOR_RGB565_GREEN);
-    Serial.println("LED ON");
     delay(1000);
 
     digitalWrite(LED_PIN, LOW);
-    display.printLine("LED OFF", 0, 30, COLOR_RGB565_RED);
-    Serial.println("LED OFF");
     delay(1000);
 }
