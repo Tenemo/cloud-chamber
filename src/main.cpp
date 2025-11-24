bddf6138c9161864fccf54f0ac2c58a5f7aca232
Tenemo <piotr@piech.dev>
2025-11-24 10:43:53 +0100

    correct screen size used

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,25 @@
 #include "Display.h"
 #include "config.h"
 #include <Arduino.h>
+#include <esp_task_wdt.h>
 
 Display display(TFT_DC, TFT_CS, TFT_RST, LCD_BL);
 
 void setup() {
     esp_log_level_set("*", ESP_LOG_ERROR);
+    // Watchdog timer set to 60 seconds,
+    // because it doesn't like file uploads.
+    esp_task_wdt_init(60, true);
     Serial.begin(115200);
     Serial.println("Initializing...");
+    display.printLine("Initializing...", 0, 0, 1);
 
     pinMode(LED_PIN, OUTPUT);
 
     display.begin();
+    display.clear();
 
-    display.printLine("Cloud Chamber", 0, 0);
+    display.printLine("Initialized.", 0, 0, 1);
 
     Serial.println("Initialized.");
 }
