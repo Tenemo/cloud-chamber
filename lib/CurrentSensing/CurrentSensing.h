c6fbe7baa19051c9d910eb46a378473190e5824d
Tenemo <piotr@piech.dev>
2025-11-26 03:27:02 +0100

    more consistent sensor libs

--- lib/CurrentSensing/CurrentSensing.h
+++ lib/CurrentSensing/CurrentSensing.h
@@ -5,7 +5,7 @@
  * USAGE:
  * ------
  * 1. Create instance:
- *    CurrentSensing current_sensors(logger);
+ *    CurrentSensing current_sensors(logger, "I1", "I2", "I_Total");
  *
  * 2. Initialize (calibration happens automatically):
  *    current_sensors.begin();
@@ -26,11 +26,12 @@
 
 class CurrentSensing {
   public:
-    CurrentSensing(Logger &logger);
+    CurrentSensing(Logger &logger, const char *label1, const char *label2,
+                   const char *labelTotal);
 
     void begin();
     void update();
-    bool isReady() const { return _sensors_calibrated; }
+    bool isReady() const { return _initialized && !_in_error_state; }
 
     float getSensor1Current() const { return _sensor1_filtered_current; }
     float getSensor2Current() const { return _sensor2_filtered_current; }
@@ -48,12 +49,19 @@ class CurrentSensing {
     static float clampSmallCurrent(float current, float threshold = 0.1f);
 
     Logger &_logger;
+    const char *_label1;
+    const char *_label2;
+    const char *_labelTotal;
+    const char *_id1;
+    const char *_id2;
+    const char *_idTotal;
 
     float _sensor1_offset_voltage;
     float _sensor2_offset_voltage;
     float _sensor1_filtered_current;
     float _sensor2_filtered_current;
-    bool _sensors_calibrated;
+    bool _initialized;
+    bool _in_error_state;
     int _adc_max_value;
     unsigned long _last_update_time;
     unsigned long _last_imbalance_warning_time;
