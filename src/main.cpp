d9116b0d55ef181cbdef5019846604070441dd81
Tenemo <piotr@piech.dev>
2025-12-09 01:01:08 +0100

    reading from a single DPS5015

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 /**
  * @file main.cpp
- * @brief Cloud chamber monitoring system - temperature sensing
+ * @brief Cloud chamber monitoring system - temperature sensing and power
+ * control
  *
  * IMPORTANT: NEVER use Serial.print/println directly in this application.
  * Always use logger.log() for all output - it handles both display and Serial.
@@ -9,6 +10,7 @@
  * - Logger: Manages TFT display (KV store + live log area) and Serial output
  * - PT100Sensor: Temperature monitoring via MAX31865 RTD interface
  * - DS18B20Sensor: Digital temperature sensors via OneWire
+ * - DPS5015: Programmable power supply control via Modbus RTU
  *
  * All modules self-manage their update timing and display registration.
  */
@@ -16,6 +18,7 @@
 #define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
 #include "esp_log.h"
 
+#include "DPS5015.h"
 #include "DS18B20.h"
 #include "Logger.h"
 #include "PT100.h"
@@ -39,6 +42,9 @@ DS18B20Sensor ds18b20_sensor_2(logger, dallasSensors, DS18B20_2_ADDRESS,
 DS18B20Sensor ds18b20_sensor_3(logger, dallasSensors, DS18B20_3_ADDRESS,
                                "TEMP_3");
 
+// DPS5015 power supply (uses Serial1 on TX/RX pins)
+DPS5015 psu(logger, "PSU");
+
 static void configureSystemLogging() {
     esp_log_level_set("*", ESP_LOG_ERROR);
     esp_task_wdt_init(60, true);
@@ -52,6 +58,8 @@ static void initializeHardware() {
     ds18b20_sensor_2.begin();
     ds18b20_sensor_3.begin();
     pt100_sensor.begin();
+    psu.begin(PIN_DPS5015_RX,
+              PIN_DPS5015_TX); // RX pin for receive, TX pin for transmit
 }
 
 void setup() {
@@ -65,4 +73,5 @@ void loop() {
     ds18b20_sensor_2.update();
     ds18b20_sensor_3.update();
     pt100_sensor.update();
+    psu.update();
 }
